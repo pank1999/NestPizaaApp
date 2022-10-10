04d195451c92fe7939097b7ad808ad0c5124d55f
pank1999 <pankajneeraj541@gmail.com>
2022-10-10 19:07:08 +0530

    created new branch

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -5,12 +5,14 @@ import { CartDto } from './dto/cart.dto';
 @Controller('cart')
 export class CartController {
     constructor(private cartService:CartService){}
-
+     
+    //get cart by user
     @Get('/:userId')
     getUserCart(@Param() userId:number){
         this.cartService.findByUserId(userId);
     }
 
+    //add Pizza to cart
     @Post()
     addPizzaToCart(@Body() cartData:CartDto){
         this.cartService.create(cartData);
